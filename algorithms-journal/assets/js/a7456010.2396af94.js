476765dd8d5ef68ada4aa2610ba84967b045ccb2
Tim Hourihan <timothyhourihan@icloud.com>
2025-09-02 22:16:32 -0400

    Remove unused directories

--- algorithms-journal/assets/js/a7456010.2396af94.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkalgorithms_journal=self.webpackChunkalgorithms_journal||[]).push([[235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- algorithms-journal/assets/js/aba21aa0.cf41fc0c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkalgorithms_journal=self.webpackChunkalgorithms_journal||[]).push([[742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
